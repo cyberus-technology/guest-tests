35956af574ee6c77ae222ca0365212e09fe0b964
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2023-09-08 16:23:31 +0200

    nix: fix cyberus-overlay according to cbspkgs guidelines

--- nix/cyberus-overlay.nix
+++ nix/cyberus-overlay.nix
@@ -1,3 +1,39 @@
-self: super:
+final: prev:
 
-super.callPackage ./release.nix self
+let
+  testNames = [
+    "cpuid"
+    "cxx"
+    "debugport"
+    "emulator"
+    "emulator-syscall"
+    "exceptions"
+    "fpu"
+    "lapic-modes"
+    "lapic-priority"
+    "lapic-timer"
+    "msr"
+    "pagefaults"
+    "sgx"
+    "sgx-launch-control"
+    "tsc"
+    "vmx"
+  ];
+
+  all = final.cyberus.guest-tests.tests.all;
+
+  extractTest = name: final.runCommand "guesttest-${name}" { } ''
+    mkdir -p $out
+    cp ${all}/tests/${name}_guesttest $out
+    cp ${all}/tests/${name}_guesttest-elf32 $out
+  '';
+
+in
+{
+  tests = {
+    all = final.callPackage ./build.nix { };
+  } // (builtins.foldl'
+    (acc: name: acc // { "${name}" = extractTest name; })
+    { }
+    testNames);
+}
