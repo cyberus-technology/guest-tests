096332b3e4146069f421e9ef36fbf225c4f34351
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2023-12-04 17:53:41 +0100

    libtoyos: print cmdline in beginning
    This is essential information to investigate failures in the future. We expect
    users of our tests (via Nix) to modify the cmdline in their projects. To identify
    the environment in that a test runs, the cmdline must be printed.

--- src/lib/toyos/src/baretest/baretest.cpp
+++ src/lib/toyos/src/baretest/baretest.cpp
@@ -21,11 +21,18 @@
  */
 
 #include <toyos/baretest/baretest.hpp>
+#include <toyos/boot_cmdline.hpp>
 
 void __attribute__((weak)) prologue()
 {}
 void __attribute__((weak)) epilogue()
 {}
+
+void print_environment_info()
+{
+    printf("Running Guest Test");
+    printf("  cmdline: %s\n", get_boot_cmdline().c_str());
+};
 
 namespace baretest
 {
