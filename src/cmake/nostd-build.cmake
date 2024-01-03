7f93b2b2a6366b088c61a6b536c640d9443ef883
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2024-01-03 15:17:26 +0100

    cmake: refactor nostd-build.cmake to a function
    This enables to provide a library toyos-hosted in the next step.

--- src/cmake/nostd-build.cmake
+++ src/cmake/nostd-build.cmake
@@ -1,32 +1,53 @@
-# Options affecting the compiler output. Removing all references to the default
-# standard libraries and enabling/disabling relevant CPU features.
+# Options affecting the build output. Removing all references to the default
+# standard libraries and enabling/disabling relevant CPU features, as well as
+# special linker configurations.
 
-add_compile_options(
-  -fcheck-new
-  -fdata-sections
-  -ffunction-sections
-  -fno-builtin
-  -fno-common
-  -fno-plt
-  -fpie
-  -fvisibility=hidden
-  -m64
-  -march=westmere
-  -mno-3dnow
-  -mno-mmx
-  -mno-sse
-  # We use push/pop in inline assembly, which is generally not safe with
-  # red zone enabled.
-  -mno-red-zone
-  -nodefaultlibs
-  -nostdinc
-  -nostdlib
-  "$<$<C_COMPILER_ID:GNU>:-fno-stack-protector>"
-  "$<$<COMPILE_LANGUAGE:CXX>:-fno-exceptions>"
-  "$<$<COMPILE_LANGUAGE:CXX>:-fno-threadsafe-statics>"
-  "$<$<COMPILE_LANGUAGE:CXX>:-fno-rtti>"
-  )
+function(add_nostd_compile_options TARGET VISIBILITY_MODIFIER)
+  if(TARGET ${TARGET})
+    target_compile_options(
+      ${TARGET}
+      ${VISIBILITY_MODIFIER}
+      -fcheck-new
+      -fdata-sections
+      -ffunction-sections
+      -fno-builtin
+      -fno-common
+      -fno-plt
+      -fpie
+      -fvisibility=hidden
+      -m64
+      -march=westmere
+      -mno-3dnow
+      -mno-mmx
+      -mno-sse
+      # We use push/pop in inline assembly, which is generally not safe with
+      # red zone enabled.
+      -mno-red-zone
+      -nodefaultlibs
+      -nostdinc
+      -nostdlib
+      "$<$<C_COMPILER_ID:GNU>:-fno-stack-protector>"
+      "$<$<COMPILE_LANGUAGE:CXX>:-fno-exceptions>"
+      "$<$<COMPILE_LANGUAGE:CXX>:-fno-threadsafe-statics>"
+      "$<$<COMPILE_LANGUAGE:CXX>:-fno-rtti>"
+      )
+  else()
+    message(WARNING "Target ${TARGET} does not exist.")
+  endif()
+endfunction()
 
-add_link_options(
-  -nostdlib -Wl,-z,max-page-size=4096 -Wl,--gc-sections -Wl,--no-relax -static
-  )
+function(add_nostd_link_options TARGET VISIBILITY_MODIFIER)
+  if(TARGET ${TARGET})
+    target_link_options(
+      ${TARGET}
+      ${VISIBILITY_MODIFIER}
+      -nostdlib
+      -Wl,-z,max-page-size=4096
+      -Wl,--gc-sections
+      -Wl,--no-relax
+      -static
+      )
+  else()
+    message(WARNING "Target ${TARGET} does not exist.")
+  endif()
+endfunction()
