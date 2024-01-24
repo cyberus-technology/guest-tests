de022f21f606f52a0098d373db115d352de8682b
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2024-01-24 16:00:39 +0100

    nix: cyberus-overlay: export all sotests and a sotest smoke test

--- nix/cyberus-overlay.nix
+++ nix/cyberus-overlay.nix
@@ -1,5 +1,9 @@
 final: _prev:
 
+let
+  sotest = import ./sotest.nix { pkgs = final; };
+  addSotestMeta = final.cyberus.cbspkgs.lib.tests.addSotestMeta;
+in
 {
   # This is just an attribute set, so we don't use callPackage. Further, it is
   # important that no `override` attribute exists here, so that the test names
@@ -13,5 +17,15 @@ final: _prev:
       gcc11
       nix-gitignore
       runCommand;
+  };
+  sotests = {
+    # SoTest bundle with all tests.
+    default = addSotestMeta sotest {
+      name = "guest-tests: all tests";
+    };
+    smoke = addSotestMeta sotest.singleRuns.hello-world {
+      name = "guest-tests: hello-world smoke test";
+      category = "smoke";
+    };
   };
 }
