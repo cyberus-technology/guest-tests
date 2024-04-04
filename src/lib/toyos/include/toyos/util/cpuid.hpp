059b460769ef2a8340497f82dfe997aa04fffd54
Markus Partheymüller <markus.partheymueller@cyberus-technology.de>
2024-04-04 17:53:10 +0200

    toyos: Add cpuid vendor id helpers

--- src/lib/toyos/include/toyos/util/cpuid.hpp
+++ src/lib/toyos/include/toyos/util/cpuid.hpp
@@ -2,6 +2,7 @@
 
 #include <array>
 #include <string>
+#include <vector>
 
 #include <toyos/x86/cpuid.hpp>
 #include <toyos/x86/x86asm.hpp>
@@ -11,6 +12,20 @@ namespace util::cpuid
     inline bool hv_bit_present()
     {
         return ::cpuid(CPUID_LEAF_FAMILY_FEATURES).ecx & LVL_0000_0001_ECX_HV;
+    }
+
+    /**
+     * Return the vendor ID string from CPUID by reading the respective leaf
+     * and combining EBX-ECX-EDX to a string.
+     */
+    inline std::string get_vendor_id()
+    {
+        std::vector<uint32_t> vendor_id_components{};
+        auto res = ::cpuid(CPUID_LEAF_MAX_LEVEL_VENDOR_ID);
+        vendor_id_components.push_back(res.ebx);
+        vendor_id_components.push_back(res.edx);
+        vendor_id_components.push_back(res.ecx);
+        return { reinterpret_cast<char*>(vendor_id_components.data()) };
     }
 
     /**
@@ -41,4 +56,13 @@ namespace util::cpuid
         return { reinterpret_cast<char*>(brand_string_components.data()) };
     }
 
+    inline bool is_intel_cpu()
+    {
+        return get_vendor_id() == "GenuineIntel";
+    }
+
+    inline bool is_amd_cpu()
+    {
+        return get_vendor_id() == "AuthenticAMD";
+    }
 }  // namespace util::cpuid
