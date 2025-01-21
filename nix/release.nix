860ce5afe09d7e725bb7c276add59598656d86c8
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2025-01-21 13:36:31 +0100

    nix: remove cbspkgs

--- nix/release.nix
+++ nix/release.nix
@@ -5,11 +5,4 @@
  */
 { pkgs ? import ./public/nixpkgs.nix }:
 
-pkgs.lib.mergeAttrs
-  (import ./public/release.nix { inherit pkgs; })
-  (if pkgs ? cyberus then
-  # This is the case if we pass in a pkgs version with applied cyberus overlays.
-    (import ./cyberus/release.nix { inherit pkgs; })
-  else
-    (import ./cyberus/release.nix { /* creates its own instance */ })
-  )
+import ./public/release.nix { inherit pkgs; }
