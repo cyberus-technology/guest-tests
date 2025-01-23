01c345a33eb4cfd360e46206125f50aec5bce1e6
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2025-01-23 09:58:56 +0100

    flake: also export testRuns as flat packages in flake
    This is necessary as packages in a flake need to be flat.
    We can't have a nested structure as we are used to from
    traditional `build.nix` files.

--- flake.nix
+++ flake.nix
@@ -17,7 +17,12 @@
       # No artificially limit here. If it builds, it builds.
       systems = inputs.nixpkgs.lib.systems.flakeExposed;
       perSystem =
-        { pkgs, system, ... }:
+        {
+          pkgs,
+          lib,
+          system,
+          ...
+        }:
         let
           project = import ./nix/release.nix {
             inherit pkgs;
@@ -37,15 +42,27 @@
           formatter = pkgs.nixfmt-rfc-style;
           packages =
             let
-              project = import ./default.nix {
-                inherit pkgs;
-                pkgs-23-11 = import inputs.nixpkgs-23-11 { inherit system; };
-              };
+              # Flattens a nested attribute set recursively, by constructing a
+              # new flat attribute key from the whole path.
+              flattenAttrsByName =
+                attrSet: prefix: sep:
+                lib.concatMapAttrs (
+                  key: value:
+                  let
+                    fullKey = "${prefix}${sep}${key}";
+                  in
+                  if lib.isAttrs value && !lib.isDerivation value then
+                    flattenAttrsByName value fullKey sep
+                  else
+                    { ${fullKey} = value; }
+                ) attrSet;
+              flattenedTestRuns = flattenAttrsByName project.testRuns "testRun" "_";
             in
             {
               inherit (project) guest-tests;
               default = project.guest-tests;
-            };
+            }
+            // flattenedTestRuns;
         };
     };
 }
